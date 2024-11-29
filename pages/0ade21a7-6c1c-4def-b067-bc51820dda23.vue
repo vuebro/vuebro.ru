e819370dd3b3c50405beb3c0bc78cc409e1b88c0
Jerry Bruwes <jbruwes@gmail.com>
2024-11-29 14:30:06 +0200

    vue

--- pages/0ade21a7-6c1c-4def-b067-bc51820dda23.vue
+++ pages/0ade21a7-6c1c-4def-b067-bc51820dda23.vue
@@ -1,20 +1,21 @@
 <template>
-<div class="flex items-center bg-cover bg-center min-h-[66dvh]" :style="`background-image:url(${the.images[0].url})`"
-    un-cloak>
-    <div class="container mx-auto px-4 py-12 place-items-center grid">
-        <div
-            class="gap-4 flex items-center justify-around w-full flex-col lg:flex-row-reverse bg-black/20 rounded-3xl shadow-2xl pa-6 backdrop-blur-sm bg-clip-border border-2 border-white/80">
-            <img src="images/vues3.svg" class="w-36 lg:w-48 rounded-full lg:ma-10" decoding="async">
-            <div class="text-center lg:text-left text-xs sm:text-sm md:text-base">
-                <h3 class="text-2xl lg:text-3xl text-slate-50 mb-6 font-['Rubik_Mono_One'] drop-shadow">{{
-                    visible
-                    }}<span class="blink underline decoration-white decoration-4 text-transparent">{{ title[length] }}</span><span class="invisible">{{ invisible }}</span>
-                </h3>
-                <el-text size="large" class="!text-slate-50 drop-shadow" tag="b">{{ the.description }}</el-text>
+    <div class="flex items-center bg-cover bg-center min-h-[66dvh]"
+        :style="`background-image:url(${the.images[0].url})`" un-cloak>
+        <div class="container mx-auto px-4 py-12 place-items-center grid">
+            <div
+                class="gap-4 flex items-center justify-around w-full flex-col lg:flex-row-reverse bg-black/20 rounded-3xl shadow-2xl pa-6 backdrop-blur-sm bg-clip-border border-2 border-white/80">
+                <img src="images/vues3.svg" class="w-36 lg:w-48 rounded-full lg:ma-10" decoding="async">
+                <div class="text-center lg:text-left text-xs sm:text-sm md:text-base">
+                    <h3 class="text-2xl lg:text-3xl text-slate-50 mb-6 font-['Rubik_Mono_One'] drop-shadow">{{
+                        visible
+                        }}<span class="blink underline decoration-white decoration-4 text-transparent">{{ title[length]
+                            }}</span><span class="invisible">{{ invisible }}</span>
+                    </h3>
+                    <el-text size="large" class="!text-slate-50 drop-shadow" tag="b">{{ the.description }}</el-text>
+                </div>
             </div>
         </div>
     </div>
-</div>
 </template>
 
 <script setup>
--- pages/5ef90475-3500-43c9-898b-dc4824f97d12.vue
+++ pages/5ef90475-3500-43c9-898b-dc4824f97d12.vue
@@ -1,87 +1,90 @@
 <template>
-<div class="min-h-dvh" un-cloak>
-    <div class="sticky not-prose top-0 z-50 pa-4 border-b bg-neutral-50 opacity-0 hover:opacity-100 transition-opacity duration-1000"
-        :class="{ 'opacity-100': !ready }" ref="pageHeader">
-        <el-page-header icon="" :content="current.title">
-            <template #breadcrumb>
-                <el-breadcrumb separator="/">
-                    <el-breadcrumb-item v-for="item in current.branch" :to="item.to">
-                        {{ item.name }}
-                    </el-breadcrumb-item>
-                </el-breadcrumb>
-            </template>
-            <template #title>
-                <router-link to="/" class="whitespace-nowrap flex items-center max-sm:hidden">
-                    <el-avatar src="images/vues3.svg" size="small"></el-avatar>
-                </router-link>
-            </template>
-            <template #extra>
-                <el-button circle @click="drawer = true">
-                    <icon icon="mdi:menu" class="size-5"></icon>
-                </el-button>
-            </template>
-        </el-page-header>
-    </div>
-    <router-view></router-view>
-</div>
-<footer class="not-prose text-slate-500" un-cloak>
-    <div class="pt-16 pb-12 text-sm border-t border-slate-200 bg-slate-100">
-        <div class="container px-6 mx-auto">
-            <div class="grid grid-cols-4 md:grid-cols-8 lg:grid-cols-12 gap-6">
-                <div class="col-span-4 md:col-span-8 lg:col-span-4 mb-3">
-                    <img class="not-prose -mt-16 w-full md:w-60 max-md:max-w-60 mx-auto" src="images/drakkar.svg" decoding="async">
-                    <h4 class="text-center text-base text-slate-700 font-medium">Сделано на берегах Балтики</h4>
-                </div>
-                <nav class="col-span-2 md:col-span-4 lg:col-span-2" v-for="{ name, children, icon } in views">
-                    <h3 class="mb-6 text-base text-slate-700 font-medium">
-                        <icon :icon="icon" class="mr-2 size-8 inline"></icon><span>{{ name }}</span>
-                    </h3>
-                    <ul>
-                        <li v-for="child in children" class="mb-2 leading-6">
-                            <router-link :to="child.to" v-if="child?.enabled"
-                                class="transition-colors duration-300 hover:text-emerald-500 focus:text-emerald-600">
-                                <icon :icon="child.icon" class="mr-2 size-6 inline"></icon><span>{{ child.name }}</span>
-                            </router-link>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-        </div>
-    </div>
-    <div class="py-4 text-sm border-t border-slate-200 bg-slate-100">
-        <div class="container px-6 mx-auto">
-            <div class="flex flex-col md:flex-row w-full gap-4 justify-between">
-                <div class="flex items-center col-span-1 gap-4 text-base font-medium leading-6"><img class="size-6 shrink-0" src="images/vues3.svg">{{ the.description }}
-                </div>
-                <nav class="text-right col-span-2 md:col-span-4 lg:col-span-6">
-                    <ul class="flex items-center justify-end gap-4">
-                        <li v-for="{icon, href} in social">
-                            <a :href="href" target="_blank" rel="noopener noreferrer"
-                                class="transition-colors duration-300 hover:text-sky-600 focus:text-sky-700">
-                                <icon class="size-6 shrink-0" :icon="icon"></icon>
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-        </div>
-    </div>
-</footer>
-<div id="drawer not-prose">
-    <el-drawer v-model="drawer" :title="the.header" class="w-full sm:w-96" size="">
-        <el-menu :router="true" :default-openeds="[0, 1, 2]">
-            <el-sub-menu v-for="({ name, $children, icon }, index) in views" :index="index">
-                <template #title>
-                    <icon :icon="icon" class="icon"></icon><span>{{ name }}</span>
+    <div class="min-h-dvh" un-cloak>
+        <div class="sticky not-prose top-0 z-50 pa-4 border-b bg-neutral-50 opacity-0 hover:opacity-100 transition-opacity duration-1000"
+            :class="{ 'opacity-100': !ready }" ref="pageHeader">
+            <el-page-header icon="" :content="current.title">
+                <template #breadcrumb>
+                    <el-breadcrumb separator="/">
+                        <el-breadcrumb-item v-for="item in current.branch" :to="item.to">
+                            {{ item.name }}
+                        </el-breadcrumb-item>
+                    </el-breadcrumb>
                 </template>
-                <el-menu-item v-for="(child, index2) in $children" :route="{ name: child.id }" :index="index2">
-                    <icon :icon="child.icon" class="icon"></icon><span>{{ child.name }}</span>
-                </el-menu-item>
-            </el-sub-menu>
-        </el-menu>
-    </el-drawer>
-</div>
-<el-backtop></el-backtop>
+                <template #title>
+                    <router-link to="/" class="whitespace-nowrap flex items-center max-sm:hidden">
+                        <el-avatar src="images/vues3.svg" size="small"></el-avatar>
+                    </router-link>
+                </template>
+                <template #extra>
+                    <el-button circle @click="drawer = true">
+                        <icon icon="mdi:menu" class="size-5"></icon>
+                    </el-button>
+                </template>
+            </el-page-header>
+        </div>
+        <router-view></router-view>
+    </div>
+    <footer class="not-prose text-slate-500" un-cloak>
+        <div class="pt-16 pb-12 text-sm border-t border-slate-200 bg-slate-100">
+            <div class="container px-6 mx-auto">
+                <div class="grid grid-cols-4 md:grid-cols-8 lg:grid-cols-12 gap-6">
+                    <div class="col-span-4 md:col-span-8 lg:col-span-4 mb-3">
+                        <img class="not-prose -mt-16 w-full md:w-60 max-md:max-w-60 mx-auto" src="images/drakkar.svg"
+                            decoding="async">
+                        <h4 class="text-center text-base text-slate-700 font-medium">Сделано на берегах Балтики</h4>
+                    </div>
+                    <nav class="col-span-2 md:col-span-4 lg:col-span-2" v-for="{ name, children, icon } in views">
+                        <h3 class="mb-6 text-base text-slate-700 font-medium">
+                            <icon :icon="icon" class="mr-2 size-8 inline"></icon><span>{{ name }}</span>
+                        </h3>
+                        <ul>
+                            <li v-for="child in children" class="mb-2 leading-6">
+                                <router-link :to="child.to" v-if="child?.enabled"
+                                    class="transition-colors duration-300 hover:text-emerald-500 focus:text-emerald-600">
+                                    <icon :icon="child.icon" class="mr-2 size-6 inline"></icon><span>{{ child.name
+                                        }}</span>
+                                </router-link>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+        <div class="py-4 text-sm border-t border-slate-200 bg-slate-100">
+            <div class="container px-6 mx-auto">
+                <div class="flex flex-col md:flex-row w-full gap-4 justify-between">
+                    <div class="flex items-center col-span-1 gap-4 text-base font-medium leading-6"><img
+                            class="size-6 shrink-0" src="images/vues3.svg">{{ the.description }}
+                    </div>
+                    <nav class="text-right col-span-2 md:col-span-4 lg:col-span-6">
+                        <ul class="flex items-center justify-end gap-4">
+                            <li v-for="{ icon, href } in social">
+                                <a :href="href" target="_blank" rel="noopener noreferrer"
+                                    class="transition-colors duration-300 hover:text-sky-600 focus:text-sky-700">
+                                    <icon class="size-6 shrink-0" :icon="icon"></icon>
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <div id="drawer not-prose">
+        <el-drawer v-model="drawer" :title="the.header" class="w-full sm:w-96" size="">
+            <el-menu :router="true" :default-openeds="[0, 1, 2]">
+                <el-sub-menu v-for="({ name, $children, icon }, index) in views" :index="index">
+                    <template #title>
+                        <icon :icon="icon" class="icon"></icon><span>{{ name }}</span>
+                    </template>
+                    <el-menu-item v-for="(child, index2) in $children" :route="{ name: child.id }" :index="index2">
+                        <icon :icon="child.icon" class="icon"></icon><span>{{ child.name }}</span>
+                    </el-menu-item>
+                </el-sub-menu>
+            </el-menu>
+        </el-drawer>
+    </div>
+    <el-backtop></el-backtop>
 </template>
 
 <script setup>
