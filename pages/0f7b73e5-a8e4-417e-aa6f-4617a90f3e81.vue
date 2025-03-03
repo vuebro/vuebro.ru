9646a9dc1fa474e943fb1b1f2f1e1a03c9f39de1
Jerry Bruwes <jbruwes@icloud.com>
2025-03-03 14:55:27 +0200

    screenshots update

--- pages/0f7b73e5-a8e4-417e-aa6f-4617a90f3e81.vue
+++ pages/0f7b73e5-a8e4-417e-aa6f-4617a90f3e81.vue
@@ -1,19 +1,16 @@
 <template>
     <el-carousel type="card" :height="height" indicator-position="outside" un-cloak>
-        <el-carousel-item v-for="({ alt, url }, i) in images" class="!h-fit"
-            v-element-size="({ height }) => { heights[i] = height }"><img :src="url" :alt="alt"
-                decoding="async"></el-carousel-item>
+        <el-carousel-item v-for="i in 6" class="!h-fit" v-element-size="({ height }) => { heights[i - 1] = height }"><img
+                :src="`images/screenshots/${i}.png`" decoding="async"></el-carousel-item>
     </el-carousel>
 </template>
 
 <script setup>
-import { inject, ref, watch } from "vue";
+import { ref, watch } from "vue";
 import { vElementSize } from "@vueuse/components";
 import { set } from "@vueuse/core";
 
-const { id } = defineProps(["id"]),
-    { images } = inject("pages")[id],
-    heights = ref([]),
+const heights = ref([]),
     height = ref(""),
     deep = true;
 
