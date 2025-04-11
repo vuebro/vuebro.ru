dbba27ea86f330b36f11a36555aa6d08db027855
Jerry Bruwes <jbruwes@icloud.com>
2025-04-11 22:38:29 +0200

    modified:   pages/0f7b73e5-a8e4-417e-aa6f-4617a90f3e81.vue

--- pages/0f7b73e5-a8e4-417e-aa6f-4617a90f3e81.vue
+++ pages/0f7b73e5-a8e4-417e-aa6f-4617a90f3e81.vue
@@ -1,18 +1,5 @@
 <template>
-    <el-carousel type="card" :height="height" indicator-position="outside" un-cloak>
-        <el-carousel-item v-for="i in 6" class="!h-fit" v-element-size="({ height }) => { heights[i - 1] = height }"><img
-                :src="`images/screenshots/${i}.png`" decoding="async"></el-carousel-item>
+    <el-carousel type="card" height="auto" indicator-position="outside" un-cloak>
+        <el-carousel-item v-for="i in 6" class="!h-fit"><img :src="`images/screenshots/${i}.png`" decoding="async"></el-carousel-item>
     </el-carousel>
 </template>
-
-<script setup>
-import { ref, watch } from "vue";
-import { vElementSize } from "@vueuse/components";
-import { set } from "@vueuse/core";
-
-const heights = ref([]),
-    height = ref(""),
-    deep = true;
-
-watch(heights, (value) => { requestAnimationFrame(() => { set(height, `${Math.max(...value)}px`) }) }, { deep });
-</script>
