f885a28b4d57791461595c05d039a3c52658b055
Jerry Bruwes <jbruwes@icloud.com>
2025-04-13 21:32:12 +0200

    2025-04-13

--- pages/0f7b73e5-a8e4-417e-aa6f-4617a90f3e81.vue
+++ pages/0f7b73e5-a8e4-417e-aa6f-4617a90f3e81.vue
@@ -1,5 +1,23 @@
 <template>
-    <el-carousel type="card" height="auto" indicator-position="outside" un-cloak>
-        <el-carousel-item v-for="i in 6" class="!h-fit"><img :src="`images/screenshots/${i}.png`" decoding="async"></el-carousel-item>
+    <el-carousel type="card" :height="Height" indicator-position="outside" un-cloak>
+        <el-carousel-item v-for="i in 6" class="!h-auto" v-element-size="elementSise"><img
+                :src="`images/screenshots/${i}.png`" decoding="async"></el-carousel-item>
     </el-carousel>
 </template>
+
+<script setup>
+import { ref, watch, onMounted } from "vue";
+import { useRoute } from "vue-router";
+import { vElementSize } from "@vueuse/components";
+
+const route = useRoute(),
+    Height = ref(""),
+    elementSise = async ({ height }) => {
+        if (height) requestAnimationFrame(() => { Height.value = `${height}px`; });
+    };
+
+onMounted(() => {
+    const element = document.getElementById(String(route.name));
+    watch(Height, () => { element.scrollIntoView({ behavior: "smooth" }) }, { once: true, flush: "post" });
+});
+</script>
