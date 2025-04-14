933e72058632b2111835901e577204dce9db8a7c
Jerry Bruwes <jbruwes@icloud.com>
2025-04-14 13:42:44 +0200

    modified:   pages/0f7b73e5-a8e4-417e-aa6f-4617a90f3e81.vue

--- pages/0f7b73e5-a8e4-417e-aa6f-4617a90f3e81.vue
+++ pages/0f7b73e5-a8e4-417e-aa6f-4617a90f3e81.vue
@@ -1,23 +1,23 @@
 <template>
-    <el-carousel type="card" :height="Height" indicator-position="outside" un-cloak>
-        <el-carousel-item v-for="i in 6" class="!h-auto" v-element-size="elementSise"><img
-                :src="`images/screenshots/${i}.png`" decoding="async"></el-carousel-item>
+    <el-carousel type="card" :height="Height" indicator-position="outside" ref="carousel" un-cloak>
+        <el-carousel-item v-for="i in srcList.length" v-element-size="elementSise" class="!h-auto"><img
+                :src="srcList[i - 1]" @click="showPreview = true"></el-carousel-item>
     </el-carousel>
+    <el-image-viewer v-if="showPreview" :url-list="srcList" show-progress :initial-index="carousel.activeIndex"
+        @close="showPreview = false"></el-image-viewer>
 </template>
 
 <script setup>
-import { ref, watch, onMounted } from "vue";
+import { ref, watch } from "vue";
 import { useRoute } from "vue-router";
 import { vElementSize } from "@vueuse/components";
 
-const route = useRoute(),
+const { name } = useRoute(),
     Height = ref(""),
-    elementSise = async ({ height }) => {
-        if (height) requestAnimationFrame(() => { Height.value = `${height}px`; });
-    };
-
-onMounted(() => {
-    const element = document.getElementById(String(route.name));
-    watch(Height, () => { element.scrollIntoView({ behavior: "smooth" }) }, { once: true, flush: "post" });
-});
+    carousel = ref(),
+    showPreview = ref(false),
+    element = document.getElementById(String(name)),
+    srcList = [...Array(6).keys()].map((key) => `images/screenshots/${key + 1}.png`),
+    elementSise = ({ height }) => { if (height) requestAnimationFrame(() => { Height.value = `${height}px` }) };
+watch(Height, () => { element.scrollIntoView({ behavior: "smooth" }) }, { once: true, flush: "post" });
 </script>
