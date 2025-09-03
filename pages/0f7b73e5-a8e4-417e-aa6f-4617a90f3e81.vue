5ec5f3329f4244797273965887e9dac8993a80ff
Jerry Bruwes <jbruwes@icloud.com>
2025-09-03 12:45:38 +0200

    2025-09-03

--- pages/0f7b73e5-a8e4-417e-aa6f-4617a90f3e81.vue
+++ pages/0f7b73e5-a8e4-417e-aa6f-4617a90f3e81.vue
@@ -1,7 +1,7 @@
 <template>
     <el-carousel type="card" :height="Height" indicator-position="outside" ref="carousel" un-cloak>
-        <el-carousel-item v-for="i in srcList.length" v-element-size="elementSise" class="!h-auto"><img
-                :src="srcList[i - 1]" @click="showPreview = true"></el-carousel-item>
+        <el-carousel-item v-for="src in srcList" v-element-size="elementSise" class="!h-auto"><img :src="src"
+                @click="showPreview = true"></el-carousel-item>
     </el-carousel>
     <el-image-viewer v-if="showPreview" :url-list="srcList" show-progress :initial-index="carousel.activeIndex"
         @close="showPreview = false"></el-image-viewer>
@@ -19,5 +19,5 @@ const { name } = useRoute(),
     element = document.getElementById(String(name)),
     srcList = [...Array(6).keys()].map((key) => `images/screenshots/${key + 1}.png`),
     elementSise = ({ height }) => { if (height) requestAnimationFrame(() => { Height.value = `${height}px` }) };
-watch(Height, () => { element.scrollIntoView({ behavior: "smooth" }) }, { once: true, flush: "post" });
+watch(Height, () => { element?.scrollIntoView({ behavior: "smooth" }) }, { once: true, flush: "post" });
 </script>
